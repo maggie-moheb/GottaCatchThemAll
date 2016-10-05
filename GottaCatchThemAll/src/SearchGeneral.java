5395f29119df51305b4334427f3012b9dbc965e6
youmna <youmna_salah@icloud.com>
2016-10-05 20:49:41 +0300

    bfs and dfs

--- GottaCatchThemAll/src/SearchGeneral.java
+++ GottaCatchThemAll/src/SearchGeneral.java
@@ -7,7 +7,7 @@ public class SearchGeneral {
 	public Node General_Search(Problem problem, QING_FUN qing_fun) {
 		Node root = new Node(); 
 		root.MakeNode(problem.initialState);
-		ArrayList<Node> nodes = new ArrayList<>();
+		ArrayList<Node> nodes = new ArrayList<Node>();
 		nodes.add(root);
 		while(true) {
 			if (nodes.size() == 0) return null; 
@@ -20,11 +20,30 @@ public class SearchGeneral {
 	// Qing function for different algorithms 
 	public ArrayList<Node> GeneralQingFunc(QING_FUN algorithm, ArrayList<Node> nodesSoFar,ArrayList<Node> nodesExpanded) {
 		if(algorithm == QING_FUN.UCS) return UniformCostSearchQing(nodesSoFar, nodesExpanded);
+		if(algorithm == QING_FUN.DFS) return DepthFirstSearchQing(nodesSoFar, nodesExpanded);
 		return null; 
+
+
 	}
 	// Qing function for uniform cost search
 	public ArrayList<Node> UniformCostSearchQing(ArrayList<Node> nodes, ArrayList<Node> newNode) {
 		return null; 
 	}
+	// Qing function for Depth first search
+	public ArrayList<Node> DepthFirstSearchQing(ArrayList<Node> nodesSoFar, ArrayList<Node> children){		
+		for(int i = 0; i<nodesSoFar.size(); i++){
+		  children.add(nodesSoFar.get(i));
+			}					
+		return children;
+	}
+	// Qing function for Breadth first search
+	public ArrayList<Node> BreadthFirstSearchQing(ArrayList<Node> nodesSoFar, ArrayList<Node> children){
+		
+		for(int i = 0; i<children.size(); i++){
+		  nodesSoFar.add(nodesSoFar.get(i));
+			}					
+		return nodesSoFar;
+	}
+	
 }
 
